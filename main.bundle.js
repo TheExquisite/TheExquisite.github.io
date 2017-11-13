321cf12cb72498b344ee8e9c68c9d07f7b304aab
Exquisite <jcjamescclare@gmail.com>
2017-11-13 17:33:49 +1000

    impactWorks test

---
+++ main.bundle.js
@@ -0,0 +1,168 @@
+webpackJsonp([1],{
+
+/***/ "../../../../../src async recursive":
+/***/ (function(module, exports) {
+
+function webpackEmptyContext(req) {
+	throw new Error("Cannot find module '" + req + "'.");
+}
+webpackEmptyContext.keys = function() { return []; };
+webpackEmptyContext.resolve = webpackEmptyContext;
+module.exports = webpackEmptyContext;
+webpackEmptyContext.id = "../../../../../src async recursive";
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+exports.push([module.i, "@import url(https://fonts.googleapis.com/css?family=Quicksand);", ""]);
+
+// module
+exports.push([module.i, ".title{\r\n    padding-top: 30vh;\r\n    font-family: Quicksand, Verdana;\r\n    font-weight: bold;\r\n    font-size: 100px;\r\n    color: #ffff;\r\n    text-shadow: 0px 3px 4px #505050\r\n}\r\n\r\n.blurbWrapper{\r\n  padding-top: 15px;\r\n  padding-bottom: 10px;\r\n  color: #ffff;\r\n  width: 60vw;\r\n  text-align: left;\r\n  font-family:  Quicksand, Verdana;\r\n}\r\n\r\n.blurb{\r\n  margin: auto;\r\n  width: 60vw;\r\n  height: 30vh;\r\n  font-family: Quicksand, Verdana;\r\n  color: #FFFF;\r\n}\r\n\r\n.learnMoreWrapper{\r\n  padding-top: 10px;\r\n  padding-bottom: 15px;\r\n  width: 60vw;\r\n  text-align: center;\r\n}\r\n\r\n.learnMoreButton{\r\n  margin: auto;\r\n  font-family: Quicksand, Verdana;\r\n  color: #FFFF;\r\n}\r\n\r\n.lineSpacer{\r\n    height: 3px;\r\n    width: 60vw;\r\n    background-color: #ffff;\r\n    box-shadow: 0px 3px 4px #505050;\r\n\r\n}\r\n\r\n.socialLinks{\r\n    padding-top: 10px;\r\n}\r\n.video-background {\r\n  position: fixed;\r\n  right: 0; \r\n  bottom: 0;\r\n  min-width: 100%; \r\n  min-height: 100%;\r\n  width: 100vw; \r\n  z-index: -100;\r\n}\r\n\r\n.iconButton{\r\n  width: 60px;\r\n  height: 60px;\r\n}\r\n\r\n.iconPadding{\r\n  padding: 10px;\r\n}\r\n\r\n.icon{\r\n  padding-top: 10px;\r\n  width: 40px;\r\n  height: 40px;\r\n}\r\n\r\n.iconWrapper{\r\n  style: inline-block;\r\n  padding:10px;\r\n  width: 60vw;\r\n  text-align: center;\r\n}\r\n\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<video autoplay loop class=\"video-background\" muted plays-inline>\r\n  <source src=\"../assets/DJI_0064.MP4\" type=\"video/mp4\">\r\n</video>\r\n\r\n<div class=title>Impact Dynamics Global</div>\r\n\r\n<div class=\"lineSpacer\"></div>\r\n\r\n<div class=\"blurbWrapper\">\r\n    What the fuck did you just fucking say about me, you little bitch? I’ll have you know I graduated top of my class in the Navy Seals, and I’ve been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I’m the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You’re fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that’s just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little “clever” comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn’t, you didn’t, and now you’re paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You’re fucking dead, kiddo.\r\n</div>\r\n<div class=\"learnMoreWrapper\">\r\n  <a class=\"learnMoreButton\" md-raised-button>Head to ___ for more infomation</a>\r\n</div>\r\n\r\n<div class=\"lineSpacer\"></div>\r\n\r\n<div class=\"iconWrapper\">\r\n  <a md-fab class=\"iconButton\" type=\"button\" href=\"http://www.google.com\">\r\n    <img class=\"icon\" src=\"../assets/facebookicon.png\" >\r\n  </a>\r\n  <span class=\"iconPadding\"></span>\r\n  <a md-fab class=\"iconButton\" type=\"button\" href=\"http://www.google.com\">\r\n    <img class=\"icon\" src=\"../assets/instaicon.png\">\r\n  </a>\r\n  <span class=\"iconPadding\"></span>\r\n  <a md-fab class=\"iconButton\" type=\"button\" href=\"http://www.google.com\">\r\n    <img class=\"icon\" src=\"../assets/twittericon.png\">\r\n  </a>\r\n  <span class=\"iconPadding\"></span>\r\n  <a md-fab class=\"iconButton\" type=\"button\" href=\"http://www.google.com\">\r\n    <img class=\"icon\" src=\"../assets/linkedinicon.png\">\r\n  </a>\r\n</div>\r\n\r\n\r\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+var AppComponent = (function () {
+    function AppComponent() {
+        this.title = 'app works!';
+    }
+    return AppComponent;
+}());
+AppComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["n" /* Component */])({
+        selector: 'app-root',
+        template: __webpack_require__("../../../../../src/app/app.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/app.component.css")]
+    })
+], AppComponent);
+
+//# sourceMappingURL=app.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/app.module.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_forms__ = __webpack_require__("../../../forms/@angular/forms.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__angular_http__ = __webpack_require__("../../../http/@angular/http.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__angular_material__ = __webpack_require__("../../../material/@angular/material.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+
+
+
+
+
+
+var AppModule = (function () {
+    function AppModule() {
+    }
+    return AppModule;
+}());
+AppModule = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
+        declarations: [
+            __WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */]
+        ],
+        imports: [
+            __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */],
+            __WEBPACK_IMPORTED_MODULE_2__angular_forms__["a" /* FormsModule */],
+            __WEBPACK_IMPORTED_MODULE_3__angular_http__["a" /* HttpModule */],
+            __WEBPACK_IMPORTED_MODULE_4__angular_material__["a" /* MdButtonModule */],
+            __WEBPACK_IMPORTED_MODULE_4__angular_material__["b" /* MdCardModule */]
+        ],
+        providers: [],
+        bootstrap: [__WEBPACK_IMPORTED_MODULE_5__app_component__["a" /* AppComponent */]]
+    })
+], AppModule);
+
+//# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/environments/environment.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The file contents for the current environment will overwrite these during build.
+var environment = {
+    production: false
+};
+//# sourceMappingURL=environment.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/main.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__ = __webpack_require__("../../../platform-browser-dynamic/@angular/platform-browser-dynamic.es5.js");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_app_module__ = __webpack_require__("../../../../../src/app/app.module.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__environments_environment__ = __webpack_require__("../../../../../src/environments/environment.ts");
+
+
+
+
+if (__WEBPACK_IMPORTED_MODULE_3__environments_environment__["a" /* environment */].production) {
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["a" /* enableProdMode */])();
+}
+__webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_platform_browser_dynamic__["a" /* platformBrowserDynamic */])().bootstrapModule(__WEBPACK_IMPORTED_MODULE_2__app_app_module__["a" /* AppModule */]);
+//# sourceMappingURL=main.js.map
+
+/***/ }),
+
+/***/ 0:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__("../../../../../src/main.ts");
+
+
+/***/ })
+
+},[0]);
+//# sourceMappingURL=main.bundle.js.map
