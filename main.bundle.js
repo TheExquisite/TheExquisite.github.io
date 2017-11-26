e969ecb838ceb25a08072057fed41b817b4afef4
Exquisite <jcjamescclare@gmail.com>
2017-11-26 13:26:07 +1000

    impactWorks final

--- main.bundle.js
+++ main.bundle.js
@@ -57,7 +57,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var AppComponent = (function () {
     function AppComponent() {
         console.log("Website Author: James Clare");
-        console.log("Iteration: 1.5.8");
+        console.log("Iteration: 1.5.9");
     }
     return AppComponent;
 }());
